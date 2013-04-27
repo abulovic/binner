f7f5dbb8ca23aa1eda98b023762286a1878b3f48
abulovic <bulovic.ana@gmail.com>
2013-04-27 18:46:54 +0100

    Appended current working dir to sys.path

--- snippets/populate_containers.py
+++ snippets/populate_containers.py
@@ -1,5 +1,6 @@
 
-import sys
+import sys, os
+sys.path.append(os.getcwd())
 from data.containers import fill_containers
 from data.containers import ReadContainer
 from data.containers import RecordContainer
@@ -8,6 +9,10 @@ from data.containers import CdsAlnContainer
 # input file: E:\\Projects\\Metagenomics\\data\\microbial\\6input_format.txt
 
 if __name__ == '__main__':
+
+	if (len(sys.argv) < 2):
+		print "Usage: python populate_containers.py <INPUT_ALN_FILE>"
+		sys.exit(-1)
 	aln_file = sys.argv[1]
 	fill_containers (aln_file)
 
