b2084d448e8683c008ae2c8de780cf5e61096aa5
abulovic <bulovic.ana@gmail.com>
2013-04-30 14:58:22 +0100

    Added finding intersection location testing.

--- test/utils/test_location.py
+++ test/utils/test_location.py
@@ -40,6 +40,30 @@ class Test(unittest.TestCase):
         assert(not location.intersects(Location.from_location((3000,4000))))
         assert(not location.intersects(Location.from_location((3000,4000), complement=True)))
 
+    def testIntersectionDifferentStrands (self):
+        loc1 = Location(complement=True)
+        self.assertEqual (None, loc1.find_intersection((0,0), False))
+
+    def testIntersectionLocation (self):
+
+        # Test no intersection case
+        loc1 = Location(sublocations=[(50,100)], complement=True)
+        self.assertEqual (None, loc1.find_intersection((200,500), True))
+        self.assertEqual (None, loc1.find_intersection((1,40), True))
+        self.assertEqual (None, loc1.find_intersection((1,50), True))
+        self.assertEqual (None, loc1.find_intersection((100,150), True))
+
+        # Test simple one interval intersection
+        loc1 = Location (sublocations=[(10,100)], complement=False)
+        loc2 = Location (sublocations=[(50,100)], complement=False)
+        self.assertEqual (loc2.sublocations, loc1.find_intersection((50,200), False).sublocations)
+
+        # Test multiple interval intersection
+        loc1 = Location (sublocations=[(1,40),(60,80),(120,200)])
+        aln_location = (30,130)
+        loc2 = Location (sublocations=[(30,40),(60,80),(120,130)])
+        self.assertEqual (loc2.sublocations, loc1.find_intersection(aln_location, False).sublocations)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testIntersections']
     unittest.main()
