709bfad86965a388cface80617ed08e182780c7a
abulovic <bulovic.ana@gmail.com>
2013-05-03 07:20:35 +0100

    Specifying BLAST format now optional. Default format offered.

--- snippets/parse_blast_output.py
+++ snippets/parse_blast_output.py
@@ -6,13 +6,18 @@ from formats.blast2input import BLASTParser
 
 if __name__ == '__main__':
 
-	if (len(sys.argv) != 4):
-	    print "Usage:\npython load_blast.py <BLAST_ALIGNMENT_FILE> <OUTPUT_FILE> <BLAST_OUTFMT>"
+	if (len(sys.argv) < 3):
+	    print "Usage:\npython load_blast.py <BLAST_ALIGNMENT_FILE> <OUTPUT_FILE> [BLAST_OUTFMT]"
+	    print "  Default BLAST format: "
+	    print "  qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
 	    sys.exit(-1)
 	    
 	alignment_fname = sys.argv[1]
 	output_fname    = sys.argv[2]
-	blast_outfmt    = sys.argv[3]
+	if len(sys.argv) != 4:
+		blast_outfmt = None
+	else:
+		blast_outfmt = sys.argv[3]
 
 	print "Loading alignment file:  {0}".format(alignment_fname)
 	print "Alignment file format:   {0}".format(blast_outfmt)
