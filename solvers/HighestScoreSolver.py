ebeb919ff2500847c18a572f30cb8c28147ba029
abulovic <bulovic.ana@gmail.com>
2013-05-03 10:12:49 +0100

    Set up database access.

--- solvers/HighestScoreSolver.py
+++ solvers/HighestScoreSolver.py
@@ -1,14 +1,12 @@
 from solvers.Solver 		import Solver
 
-from data.containers.read 	import ReadContainer
-from data.containers.cdsaln import CdsAlnContainer
+from data.containers.load	import initialize_containers
 
 class HighestScoreSolver (Solver):
 
 	def __init__ (self, alignment_file):
 
-		readContainer   = ReadContainer.Instance()
-		cdsAlnContainer = CdsAlnContainer.Instance()
-		super(HighestScoreSolver, self).__init__ (readContainer, cdsAlnContainer)
+		(readCont, recordCont, cdsAlnCont) = initialize_containers()
+		super(HighestScoreSolver, self).__init__ (readCont, cdsAlnCont)
 
 		self.readContainer.populate_from_aln_file (alignment_file)
