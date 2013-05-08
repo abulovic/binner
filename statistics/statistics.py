59fc5f588b71a92d19a104a50443bfc0626f207d
Martin Sosic <sosic.martin@gmail.com>
2013-05-08 19:12:22 +0200

    Added function definitions

---
+++ statistics/statistics.py
@@ -0,0 +1,17 @@
+def num_reads_with_no_alignments():
+    pass
+
+def num_reads_with_host_and_parasit_alignments():
+    pass
+
+def num_reads_with_no_aligned_cdss():
+    pass
+
+def num_reads_with_multiple_aligned_cdss():
+    pass
+
+def num_reads_with_multiple_mapped_cds_sublocations():
+    pass
+
+def average_cds_coverage():
+    pass
