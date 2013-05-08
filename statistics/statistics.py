ff419bbc946e468adf2b05baa923b20d459e9a22
Martin Sosic <sosic.martin@gmail.com>
2013-05-08 19:16:18 +0200

    Podjelio zadatke

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -1,17 +1,23 @@
+# Matija
 def num_reads_with_no_alignments():
     pass
 
+# Cekati da Ana napravi podrsku
 def num_reads_with_host_and_parasit_alignments():
     pass
 
+# Martin
 def num_reads_with_no_aligned_cdss():
     pass
 
+# Martin
 def num_reads_with_multiple_aligned_cdss():
     pass
 
+# Matija
 def num_reads_with_multiple_mapped_cds_sublocations():
     pass
 
+# Matija
 def average_cds_coverage():
     pass
