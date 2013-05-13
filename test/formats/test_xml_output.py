0f67580ebf1758fd7a33b16bc5b010d2659efc8e
ijerkovic <igor.jerkovich@gmail.com>
2013-05-13 19:53:29 +0200

    Updated unittests for xml

--- test/formats/test_xml_output.py
+++ test/formats/test_xml_output.py
@@ -6,7 +6,7 @@ from formats.xml_output import *
 
 class XMLOutputTest(unittest.TestCase):
 
-    def testXMLOutput(self):
+    def setUp(self):
         dataset = Dataset("Example2.fq", "Homo2", "sapiens", "human2", "9696",
                 "eukaryota, ...; Homo", "Whole Blood2", "DNA", "single-end", "Roche 454")
 
@@ -30,14 +30,29 @@ class XMLOutputTest(unittest.TestCase):
 
         # organisms now consist of host organism and 8 same parasite organisms
 
-        xml = XMLOutput(dataset, organisms)
+        self.xml = XMLOutput(dataset, organisms)
         # init with xml data: dataset and organisms 
+
+    def testDataset(self):
+        check = self.xml.dataset.name
+        self.assertIsNotNone(check, "Dataset name should be always set")
+        self.assertNotEqual(len(check), 0, "Dataset name length must be > 0")
+    
+    def testHost(self):
+        check = self.xml.dataset.host_genus
+        self.assertIsNotNone(check, "Host name should be always set")
+        self.assertNotEqual(len(check), 0, "Host name length must be > 0")
+
+    def testOrganisms(self):
+        self.assertIsInstance(self.xml.organisms, list, "Type for organisms should be list")
+
+    def testXMLOutput(self):
 
         orig_stdout = sys.stdout
         out_file = file('test/formats/test_file_xml', 'w')
         sys.stdout = out_file
         # redirect print output to file
-        xml.xml_output()
+        self.xml.xml_output()
         sys.stdout = orig_stdout
         out_file.close()
         self.assertEqual (True, filecmp.cmp('test/formats/test_file_xml', 'test/formats/xml/test_out.xml'))
