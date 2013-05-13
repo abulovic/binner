e6dced795ea6608c5ecb4972db63f269fb5da17f
abulovic <bulovic.ana@gmail.com>
2013-05-13 21:26:17 +0100

    Fixed populating read2cds.

--- data/containers/cdsaln.py
+++ data/containers/cdsaln.py
@@ -1,8 +1,8 @@
+from collections            import defaultdict
+
 from utils.singleton        import Singleton
 from data.containers.record import RecordContainer
 from data.alignment         import CdsAlignment
-
-from collections            import defaultdict
 
 @Singleton
 class CdsAlnContainer (object):
@@ -21,32 +21,38 @@ class CdsAlnContainer (object):
     
 
     def populate (self, read_cont):
-
+        '''
+        Populates CDS container from read container. 
+        Iterates through every read, and for each read it goes through 
+        all the read alignments. For each alignment, it goes through all the
+        locations where the alignments intersects a CDS.
+        Each intersected cds will be contained within a CdsALignment object
+        where all the reads mapped to the particular cds are contained. 
+        It is possible to extract cds alignment by using cds as a key, and
+        it is also possible to find all the CdsAlignments cointaining a 
+        particular read using the read identifier as a key.
+        '''
         # Iterate through reads
         for read in read_cont.read_repository.values():
-
             # Iterate through read alignments
             for readAln in read.alignment_locations:
-
                 if not readAln.active:
                     continue
-                
                 # Iterate through aligned CDSs for each read alignment
                 for (cds, alignment_location) in readAln.aligned_cdss:
                     # if this CDS hasn't been added yet
                     if not self.cds_repository.has_key (cds):
-                        cds_alignment = CdsAlignment (cds)
+                        cds_alignment            = CdsAlignment (cds)
                         cds_alignment.add_aligned_sublocation (read.id, alignment_location, readAln.score)
                         self.cds_repository[cds] = cds_alignment
-                        self.read2cds[read.id].append(cds_alignment)
-                    # CDS already exists in the cds repository
                     else:
-                        cds_alignment = self.cds_repository[cds]
-                        # CDS already contains subalignment with this read, pass 
+                        cds_alignment            = self.cds_repository[cds]
+                        # check whether CDS alignment contains this read or not
                         if cds_alignment.contains_read (read.id):
                             continue
                         else:
                             cds_alignment.add_aligned_sublocation (read.id, alignment_location, readAln.score)
+                    self.read2cds[read.id].append(cds_alignment)
 
     def __str__(self):
         tab = " " * 2
