f823bb5a64a34565a5dfd80df9c0c8fe2f2caa47
abulovic <bulovic.ana@gmail.com>
2013-05-14 17:37:00 +0100

    Added populate method that fetches all the records reported in read alignments.

--- data/containers/record.py
+++ data/containers/record.py
@@ -1,21 +1,38 @@
-from utils.singleton import Singleton
 
-@Singleton
 class RecordContainer (object):
     ''' Serves as a local Record Repository.
         If a GenBank/EMBL/DDBJ record has already been 
         fetched from the database, it can be fetched localy
         from the record repository.
     '''
+    
     def __init__ (self):
         self.record_repository  = {}
         
     def set_db_access(self, db_query):
+        '''
+        @param: db_query (DbQuery, MockDbQuery)
+        '''
+        assert (has_attr(db_query, 'get_record'))
         self.db_query = db_query
 
-    def fetch_record (self, record_id):
-        self._add_record(record_id)
-        return self.record_repository[record_id]
+    def populate (self, read_container):
+        '''
+        Populates the record container with all the records 
+        that have produced significant alignments
+        '''
+        reads = read_container.fetch_all_reads(format=iter)
+        for read in reads:
+            for read_alignment in read.get_alignments(format=iter):
+                self.fetch_record(read_alignment.nucleotide_accession)
+
+    def fetch_record (self, nucleotide_accession):
+        '''
+        @param nucleotide_accession (str)
+        @return Record (ncbi/db/[genbank/embl])
+        '''
+        self._add_record(nucleotide_accession)
+        return self.record_repository[nucleotide_accession]
         
     def _add_record (self, record_id):
         ''' Adds the record from database if not already present
