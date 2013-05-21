5e324d29737197ca878961d06a3bfb1f646af8e7
abulovic <bulovic.ana@gmail.com>
2013-05-21 21:57:35 +0200

    New taxonomy solver

---
+++ snippets/simplejoin_taxonomy_solver_test.py
@@ -0,0 +1,81 @@
+import sys, os
+sys.path.append(os.getcwd())
+
+from ncbi.db.access         import DbQuery
+from data.containers.record import RecordContainer    
+from data.containers.read   import ReadContainer    
+from data.containers.cdsaln import CdsAlnContainer
+
+from solver.read2cds.Read2CDSSolver import Read2CDSSolver
+from solver.read2cds.GreedySolver import GreedySolver
+from solver.taxonomy.SimpleJoinTaxonomySolver import SimpleJoinTaxonomySolver
+from solver.Solver import Solver
+
+from solver.determine_host import determine_host
+
+import statistics.statistics as stats
+
+if __name__ == '__main__':
+
+    if (len(sys.argv) < 2):
+        print "Solver usage: python populate_containers.py <INPUT_ALN_FILE>"
+        sys.exit(-1)
+    aln_file = sys.argv[1]
+
+    print "I got the input file!"
+
+        # # ---------------------- Initialize containers --------------------- #
+
+        # # Enable database access
+        # dbQuery = DbQuery()
+
+        # # Create containers
+        # recordCont = RecordContainer.Instance()
+        # recordCont.set_db_access(dbQuery)
+
+        # readCont   = ReadContainer.Instance()
+        # cdsAlnCont = CdsAlnContainer.Instance()
+
+        # readCont.populate_from_aln_file(aln_file)
+
+        # print "passed container initialization!"
+        
+        # ---------------------- Initialize Solver --------------------- #
+
+    greedySolver    = GreedySolver()
+    taxonomySolver  = SimpleJoinTaxonomySolver() 
+    solver = Solver(determine_host, greedySolver, taxonomySolver)
+                        
+
+        # ---------------------- Run Solver --------------------- #
+
+    solver.generateSolutionXML(aln_file)
+
+    print "Successfully initialized solver!"
+        
+	# fill_containers (aln_file)
+
+        # -------------------- Test stats methods ----------------------- #
+'''
+    read_container = ReadContainer.Instance()
+    cds_aln_container = CdsAlnContainer.Instance()
+	
+    no_alns = stats.num_reads_with_no_alignments(read_container)
+    print "Reads with no alignments: %d" % no_alns
+    print "Number of reads: %d" % stats.num_reads(read_container)
+
+    print "Number of reads with no aligned cdss: %d" % \
+	  stats.num_reads_with_no_aligned_cdss(read_container, cds_aln_container)
+
+    print "Number of reads with host and parasit alignments: %d" % \
+	      stats.num_reads_with_host_and_parasit_alignments(cds_aln_container)
+
+    print "Number of reads with multiple aligned cdss: %d" % \
+	      stats.num_reads_with_multiple_aligned_cdss(cds_aln_container)
+
+    average, deviation = stats.calc_average_cds_coverage(cds_aln_container)
+    print "Average and deviation of cds coverage: %f %f" % (average, deviation)
+	
+    more_sublocs = stats.num_reads_with_multiple_mapped_cds_sublocations(read_container)
+    print "Reads aligned to more sublocs of CDS: %d" % more_sublocs
+'''
---
+++ solver/taxonomy/SimpleJoinTaxonomySolver.py
@@ -0,0 +1,69 @@
+from TaxonomySolver import TaxonomySolver
+from collections import defaultdict
+
+
+class SimpleJoinTaxonomySolver (TaxonomySolver):
+    '''
+    If the organism rank is too low (not species, but assigned
+    a specific sequencing ID), it is pushed up in the taxonomy tree
+    to the first species node.
+    '''
+
+    def map_cdss_2_species (self, db_access, tax_tree, read_container, cds_aln_container):
+
+        assigned_taxids = defaultdict(list)
+        cds_alns = cds_aln_container.fetch_all_cds_alns()
+        ref_taxids = {}
+
+        for cds_aln in cds_alns:
+            if cds_aln.is_active():
+                
+                # 1. fetch taxonomy ID
+                taxid = self.get_taxid(cds_aln, db_access)
+# >>>>>>>>>>> LOG THIS
+                if not taxid:
+                    continue
+                # 2. determine tax ID to which this taxid maps
+                if ref_taxids.has_key(taxid):
+                    ref_taxid = ref_taxids[taxid]
+                else:
+                    ref_taxid = self.get_ref_taxid (taxid, db_access, tax_tree)
+                    ref_taxids[taxid] = ref_taxid
+                # 3. report this cds for referent taxid
+                assigned_taxids[ref_taxid].append(cds_aln)
+
+        return assigned_taxids
+
+    def get_taxid (self, cds_aln, db_access):
+        record = cds_aln.cds.record
+        taxid = None
+        try:
+            gi = record.gi
+        except AttributeError, e:
+            print "SimpleJoinTaxonomySolver", e
+        try:
+            [taxid] = db_access.get_taxids([int(gi)], format=list)
+        except TypeError, e:
+            print "SimpleJoinTaxonomySolver", e
+        return taxid
+
+    def get_ref_taxid(self, taxid, db_access, tax_tree):
+        organism_name = db_access.get_organism_name (taxid)
+        if not organism_name:
+            return taxid
+        name_parts = organism_name.split()
+        if len(name_parts) <= 2:
+            return taxid
+
+        ref_name = organism_name
+        lineage = tax_tree.get_taxonomy_lineage(taxid, db_access)
+        lineage.reverse()
+        for tax_name in lineage:
+            name_parts = tax_name.split()
+            if len(name_parts) <= 2:
+                ref_name = tax_name
+                break
+        taxid = db_access.get_organism_taxid (ref_name)
+        return taxid
+        
+            
