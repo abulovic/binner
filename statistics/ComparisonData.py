c5a9cd245f3372dc0b3600a205a52bd8bb0f8652
Martin Sosic <sosic.martin@gmail.com>
2013-06-04 23:32:56 +0200

    Changed params of comparison data

--- statistics/ComparisonData.py
+++ statistics/ComparisonData.py
@@ -3,11 +3,17 @@ class ComparisonData (object):
         one of solver phases.
     """
 
-    def __init__(self, solution_data, ostali_potrebni_parametri_idu_ovdje):
+    def __init__(self, solution_data, record_cont, read_cont, cds_aln_cont=None,
+                 taxid2cdss=None):
         """ Takes solution data and data that represents current state of solver,
         compares them and stores comparison result in self.
         @param (SolutionData) solution_data  Contains solution data.
+        @param (RecordContainer) record_cont
+        @param (ReadContainer) read_cont
+        @param (CdsAlnContainer) cds_aln_cont
+        @param (dict) taxid2cdss  Result of TaxonomySolver
         """
+
         # Ovdje izracunati rezultate usporedbe i pospremiti u atribute.
         # One koji se ne mogu izracunati jer fale podaci postavi se na None.
         self.cds_comparison = None
