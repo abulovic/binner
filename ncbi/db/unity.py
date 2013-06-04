b40ea7ac7c7e30dc977f50dcedbe17f419af64ed
Marin <marin007@gmail.com>
2013-06-04 22:11:22 +0200

    Changed UnityCDS to optimize attribute usage
    The atributes are now fixed and in the __slots__ attirbute. This
    otimizes memory usage and atribute retrieval.

--- ncbi/db/unity.py
+++ ncbi/db/unity.py
@@ -7,21 +7,6 @@ Created on May 27, 2013
 import sys
 from utils.location import Location
 
-standard_fields =[
-    'id',
-    'db'
-    'version',
-    'nucl_gi',
-    'taxon',
-    'location',
-    'protein_id',
-    'locus_tag',
-    'product',
-    'gene',
-    'prot_gi',
-    'cds'
-]
-
 class UnityRecord(object):
     def __init__(self, version):
         self.version = version
@@ -31,20 +16,42 @@ class UnityRecord(object):
         self.cds.append(cds)
 
 class UnityCDS(object):
+    __slots__ =  (
+        'id',
+        'db'
+        'version',
+        'nucl_gi',
+        'taxon',
+        'location',
+        'protein_id',
+        'locus_tag',
+        'product',
+        'gene',
+        'prot_gi',
+        'cds',
+        'origin',
+        'record_id', #compatibility issue
+        'location_min' #optimization issue        
+    )
+    
     def __init__(self, attributes={}):
-        self.attributes = attributes
         self.origin = None
+        self.id = attributes.get('id')
+        self.db = attributes.get('db')
+        self.version = attributes.get('version')
+        self.nucl_gi = attributes('nucl_gi')
+        self.taxon = attributes.get('taxon')
+        self.location = attributes.get('location')
+        self.protein_if = attributes.get('protein_id')
+        self.locus_tag = attributes.get('locus_tag')
+        self.product = attributes.get('product')
+        self.gene = attributes.get('gene')
+        self.prot_gi = attributes.get('prot_gi')
+        self.cds = attributes.get('cds')
         self.record_id = self.version #Added because of compatibility with older code
+                
         if self.location:
             self.location_min = Location.fast_min_str(self.location)
         else:
             self.location_min = sys.maxint
-        
-    def __getattr__(self, name):
-        if self.attributes.has_key(name):
-            return self.attributes[name]
-        elif name in standard_fields:
-            return None
-        else:
-            raise AttributeError("'%s' object has no attribute '%s'",
-                self.__class__.__name__, name)
+
